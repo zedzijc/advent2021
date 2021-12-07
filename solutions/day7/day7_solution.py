62489c47d634b9ecb15a4e21f9444f32f8314985
Anders Axner <aaxn@netcompany.com>
2021-12-07 15:25:43 +0100

    day 7 2 solution

--- solutions/day7/day7_solution.py
+++ solutions/day7/day7_solution.py
@@ -1,5 +1,6 @@
 from solutions.common.util import strings_to_integers
 from solutions.common.parse import parse_csv_to_list
+import math
 
 
 def find_lowest_fuel_position(puzzle_input):
@@ -13,12 +14,12 @@ def find_lowest_fuel_position(puzzle_input):
 def _calculate_by_granularity(positions, granularity, lower_bound, upper_bound):
 	value = lower_bound
 	out_of_bounds = upper_bound
-	lowest_fuel_sum = out_of_bounds * len(positions)
+	lowest_fuel_sum = _get_growing_fuel_cost(max(positions))*len(positions)
 	lowest_value = out_of_bounds
 	while value < out_of_bounds:
 		value_sum = 0
 		for position in positions:
-			value_sum += abs(position-value)
+			value_sum += _get_growing_fuel_cost(abs(position-value))
 		if value_sum < lowest_fuel_sum:
 			lowest_fuel_sum = value_sum
 			lowest_value = value
@@ -26,3 +27,8 @@ def _calculate_by_granularity(positions, granularity, lower_bound, upper_bound):
 	return lowest_value, lowest_fuel_sum
 
 
+def _get_growing_fuel_cost(number):
+	if number % 2 == 0:
+		return number * (number / 2) + number / 2
+	else:
+		return number * math.ceil(number / 2)
