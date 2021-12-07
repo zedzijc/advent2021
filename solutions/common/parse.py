2669f03f9bae0401a39da85d61227e1674cca41f
Anders Axner <aaxn@netcompany.com>
2021-12-07 11:45:03 +0100

    day 5 1 with base parsers for coordinates

--- solutions/common/parse.py
+++ solutions/common/parse.py
@@ -1,6 +1,7 @@
 from solutions.common.util import strings_to_integers, strings_to_binary
-from solutions.day4.bingo_number import numbers_to_bingo_numbers
+from solutions.common.coordinate import Coordinate
 import csv
+import re
 
 
 def parse_csv_to_list(puzzle_input):
@@ -18,3 +19,13 @@ def parse_input_as_integer_list(puzzle_input):
 
 def parse_input_as_binary_list(puzzle_input):
     return strings_to_binary(parse_input_as_list(puzzle_input))
+
+
+def parse_coordinates(string):
+    pattern = "[0-9]+,[0-9]+"
+    coordinates = []
+
+    for raw_coord in re.findall(pattern, string):
+        split_coord = raw_coord.split(',')
+        coordinates.append(Coordinate(int(split_coord[0]), int(split_coord[1])))
+    return coordinates
--- solutions/day5/day5_solution.py
+++ solutions/day5/day5_solution.py
@@ -1,4 +1,46 @@
-import re
+from solutions.common.parse import parse_input_as_list, parse_coordinates
+from solutions.common.coordinate import Coordinate
 
-def _parse_coordinates(puzzle_input):
-	pattern = "[0-9]+,[0-9]+"
+
+def count_overlapping_lines(puzzle_input):
+	print ("Number of overlapping lines: {0}".format(
+		_determine_overlapping_lines(puzzle_input)))
+
+
+def _determine_overlapping_lines(puzzle_input):
+	markings = {}
+	coordinates = _process_coordinates(parse_input_as_list(puzzle_input))
+	for coordinate_pair in coordinates:
+		for line_coordinate in _get_line_coordinates(coordinate_pair):
+			if line_coordinate not in markings:
+				markings[line_coordinate] = 1
+			else:
+				markings[line_coordinate] += 1
+
+	return len([value for value in markings.values() if value >= 2])
+
+
+def _process_coordinates(puzzle_input):
+	coordinates = []
+	for input_line in puzzle_input:
+		coordinates.append(parse_coordinates(input_line))
+	return coordinates
+
+
+def _get_line_coordinates(coordinate_pair):
+	line_coordinates = []
+	coord_a = coordinate_pair[0]
+	coord_b = coordinate_pair[1]
+
+	if coord_a == coord_b:
+		line.coordinates.append(Coordinate(coord_a.x, coord_a.y))
+
+	elif coord_a.x == coord_b.x:
+		y_coordinates = [coord_a.y, coord_b.y]
+		for y_coordinate in range(min(y_coordinates), max(y_coordinates) + 1):
+			line_coordinates.append(Coordinate(coord_a.x, y_coordinate))
+	elif coord_a.y == coord_b.y:
+		x_coordinates = [coord_a.x, coord_b.x]
+		for x_coordinate in range(min(x_coordinates), max(x_coordinates) + 1):
+			line_coordinates.append(Coordinate(x_coordinate, coord_a.y))
+	return line_coordinates
