2dfb346766a37ada47bc68bb44afe82fcc4a8224
Anders Axner <aaxn@netcompany.com>
2021-12-07 12:55:20 +0100

    day 5 2 solution

--- solutions/day5/day5_solution.py
+++ solutions/day5/day5_solution.py
@@ -35,12 +35,37 @@ def _get_line_coordinates(coordinate_pair):
 	if coord_a == coord_b:
 		line.coordinates.append(Coordinate(coord_a.x, coord_a.y))
 
-	elif coord_a.x == coord_b.x:
+	elif coord_a.x == coord_b.x: # Horizontal
 		y_coordinates = [coord_a.y, coord_b.y]
 		for y_coordinate in range(min(y_coordinates), max(y_coordinates) + 1):
 			line_coordinates.append(Coordinate(coord_a.x, y_coordinate))
-	elif coord_a.y == coord_b.y:
+	elif coord_a.y == coord_b.y: # Vertical
 		x_coordinates = [coord_a.x, coord_b.x]
 		for x_coordinate in range(min(x_coordinates), max(x_coordinates) + 1):
 			line_coordinates.append(Coordinate(x_coordinate, coord_a.y))
+	else: #Diagonal 45 degrees
+		x = coord_a.x
+		y = coord_a.y
+		if (coord_a.x > coord_b.x and coord_a.y > coord_b.y):
+			while coord_b not in line_coordinates:
+				line_coordinates.append(Coordinate(x,y))
+				x -= 1
+				y -= 1
+
+		elif (coord_a.x < coord_b.x and coord_a.y < coord_b.y):
+			while coord_b not in line_coordinates:
+				line_coordinates.append(Coordinate(x,y))
+				x += 1
+				y += 1
+		elif (coord_a.x > coord_b.x):
+			while coord_b not in line_coordinates:
+				line_coordinates.append(Coordinate(x,y))
+				x -= 1
+				y += 1
+		else:
+			while coord_b not in line_coordinates:
+				line_coordinates.append(Coordinate(x,y))
+				x += 1
+				y -= 1
+
 	return line_coordinates
